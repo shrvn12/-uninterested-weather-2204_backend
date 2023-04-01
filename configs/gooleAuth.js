c7bd5301501a3a559c50ecfb48bd6c0b75e3b1cf
shrvn12 <singhshravan1208@gmail.com>
2023-04-01 15:41:55 +0530

    changed redirect link

--- configs/gooleAuth.js
+++ configs/gooleAuth.js
@@ -8,7 +8,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:4500/auth/google/callback",
+      callbackURL: "https://tooth-tracker.cyclic.app/auth/google/callback",
     },
     async function (accessToken, refreshToken, profile, cb) {
       let email = profile._json.email;
